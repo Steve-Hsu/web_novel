0261e4be86bc348719a74ce4316c60718466893e
Steve-Hsu <wuujdsc@gmail.com>
2021-11-28 11:39:28 +0800

    mqrquee setting

---
+++ client/src/components/novelHome/NH_2_1_Marquee.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NH_2_1_Marquee = () => {
+  return (
+    <section className="marquee">
+      <span className="text">
+        整點上車 17:00 - 不要忘記了
+      </span>
+      <span className="text">
+        整點上車 17:00 - 不要忘記了
+      </span>
+    </section>
+  )
+}
+
+export default NH_2_1_Marquee
--- client/src/components/novelHome/NH_2_RightBar.js
+++ client/src/components/novelHome/NH_2_RightBar.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
+//Components
+import NH_2_1_Marquee from './NH_2_1_Marquee'
+
 function NH_2_RightBar() {
   return (
     <div className='grid-NH_RightBar'>
-      right bar
+      <NH_2_1_Marquee />
+      <div className='grid-NH_RBar_T'>333</div>
+      <div className='grid-NH_RBar_B'>444</div>
     </div>
   )
 }
